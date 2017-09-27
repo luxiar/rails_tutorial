b6ffaf66106aaf63ea3eae07e00e067952d2a3c7
NaohiroTakahashi <naohiro.takahashi@luxiar.com>
2017-09-27 17:34:41 +0900

    rails tutorial section 8

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,9 @@ class ActiveSupport::TestCase
   # -- they do not yet inherit this setting
   fixtures :all
 
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
