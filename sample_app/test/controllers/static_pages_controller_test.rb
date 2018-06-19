bf0bf71034f0c5112fb1ccb3810dee773c8711df
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-06-19 09:16:49 +0900

    chapter6

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -27,6 +27,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact|#{@base_title}"
   end
 end
