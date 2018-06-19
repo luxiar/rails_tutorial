22bbf2da80df16dbfc886773b9f9d7dd54ef5049
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-06-19 16:48:33 +0900

    chapter7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/about',  to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'application#hello'
+  get 'users/:id',   to:'users#show'
+  post '/signup',    to: 'users#create'
+#  get  '/users/new',     to: 'users#new'
+#  post '/users',     to: 'users#create', :as => 'user'
+  resource :users
 end
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180618012148) do
 
-  create_table "microposts", force: :cascade do
-   t.text "content"
-   t.integer "user_id"
-   t.datetime "created_at", null: false
-   t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
