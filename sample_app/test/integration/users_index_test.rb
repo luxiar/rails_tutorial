2b39abb3b5b92773b5f87676fc4573573f554e4e
sharsukhchuluun <sukhchuluun@luxiar.com>
2018-06-22 18:47:42 +0900

    chapter14

--- sample_app/test/integration/users_index_test.rb
+++ sample_app/test/integration/users_index_test.rb
@@ -6,11 +6,11 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
+  test "index a admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
+    assert_select 'div.pagination', count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
@@ -29,5 +29,3 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 end
-
-
