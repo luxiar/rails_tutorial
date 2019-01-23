a28c7241316754b44396a056a9714d7ad70f6f7c
koshiyashio <shio.koshiya@luxiar.com>
2019-01-23 16:30:53 +0900

    Chapter10

--- environment/sample_app/test/test_helper.rb
+++ environment/sample_app/test/test_helper.rb
@@ -3,12 +3,23 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  include ApplicationHelper
 
   def is_logged_in?
     !session[:user_id].nil?
   end
-  include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
+
+# class ActionDispatch::IntegrationTest
+
+#   # テストユーザーとしてログインする
+#   def log_in_as
+#   end
+# end
+#第９章未習のため
