9a13b8a7719eba4256f4148a6ebbd2593fa9b9ac
koshiyashio <shio.koshiya@luxiar.com>
2019-01-23 19:39:39 +0900

    Chapter13

--- environment/sample_app/test/integration/users_signup_test.rb
+++ environment/sample_app/test/integration/users_signup_test.rb
@@ -13,7 +13,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
-    assert_select 'form[action="/signup"]'
   end
 
   test "valid signup information" do
@@ -29,3 +28,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
   end
 end
+
--- environment/sample_app/test/models/user_test.rb
+++ environment/sample_app/test/models/user_test.rb
@@ -53,12 +53,20 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = " " * 6
+    @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
   end
 
   test 'password should have a minimum length' do
-    @user.password = @user.password_confirmation = "a" * 5
+    @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
+  end
+
+  test 'associated microposts should be destroyed' do
+    @user.save
+    @user.microposts.create!(content: 'Lorem ipsum')
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
