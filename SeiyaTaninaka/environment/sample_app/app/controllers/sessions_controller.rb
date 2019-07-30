fb0b6631b698f0ccd396c26a26389764d6980884
SeiyaTaninaka <seiya.taninaka@luxiar.com>
2019-07-30 16:33:41 +0900

    chapter10

--- SeiyaTaninaka/environment/sample_app/app/controllers/sessions_controller.rb
+++ SeiyaTaninaka/environment/sample_app/app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
