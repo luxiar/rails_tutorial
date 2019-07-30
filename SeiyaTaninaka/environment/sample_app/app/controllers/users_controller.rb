f24fcdb3f6da1a41d65aa582b31519cff9c791ba
SeiyaTaninaka <seiya.taninaka@luxiar.com>
2019-07-30 13:45:05 +0900

    chapter8

--- SeiyaTaninaka/environment/sample_app/app/controllers/users_controller.rb
+++ SeiyaTaninaka/environment/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
@@ -8,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
+                                   :password_confirmation)
     end
 end
