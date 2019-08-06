82e345539f68b4530fe32cf0785c42c4f1b69c43
Yuri Kimura <yuri.kimura@luxiar.com>
2019-08-06 09:07:31 +0900

    chapter10

--- YuriKimura/environment/sample_app/app/helpers/sessions_helper.rb
+++ YuriKimura/environment/sample_app/app/helpers/sessions_helper.rb
@@ -4,6 +4,18 @@ module SessionsHelper
    session[:user_id] = user.id
   end
 
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
  # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     if session[:user_id]
@@ -17,8 +29,19 @@ module SessionsHelper
   end
 
   # 現在のユーザーをログアウトする
-def log_out
-  session.delete(:user_id)
-  @current_user = nil
-end
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
